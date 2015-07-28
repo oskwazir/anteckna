d8dc30e91823a6ae1ef393f17a4615e185a210db
Omer Wazir <omer.wazir@gmail.com>
2015-07-28 15:21:37 -0700

    Add route to handle profiles

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,14 @@
 const React = require('react');
 const Main = require('../components/Main');
 const Home = require('../components/Home');
+const Profile = require('../components/Profile');
 const Router = require('react-router');
 const DefaultRoute = Router.DefaultRoute;
 const Route = Router.Route;
 
 module.exports = (
   <Route name="app" path="/" handler={Main}>
+    <Route name="profile" path="profile/:username" handler={Profile} />
     <DefaultRoute handler={Home} />
   </Route>
 )
