c40413ed62e894c046b6aba85b4b637c059e7638
Omer Wazir <omer.wazir@gmail.com>
2015-07-28 12:13:41 -0700

    Add basic home component

---
+++ app/components/Home.js
@@ -0,0 +1,10 @@
+const React = require('react');
+
+class Home extends React.Component{
+  render(){
+    return ( <h2 className="text-center">Search by Github Username Above</h2>
+      )
+  }
+}
+
+module.exports = Home;
